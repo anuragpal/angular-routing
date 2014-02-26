10f62b0f0444b857a7f9f4dfd1378c49efcae507
Anurag Pal <developer.anurag@gmail.com>
2014-02-26 13:11:31 -0800

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularRoutingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
